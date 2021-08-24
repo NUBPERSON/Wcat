7af1ad4be5eb17be52c485a599850e0f3694a544
jaivinsphogaat <jaivinsphogaat@gmail.com>
2021-08-24 16:40:48 +0530

    completed

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 #!/usr/bin/env node
-const fs=require("fs");
-let arguments=process.argv.slice(2);  // first two argument are file names
+const fs = require("fs");
+let arguments = process.argv.slice(2);  // first two argument are file names
 
-let flags= [];
-let filenames= [];
-let secondaryarg=[];
-let cnt=0;
-for(let i of arguments){
-    if(i=="-cpy"){
+let flags = [];
+let filenames = [];
+let secondaryarg = [];
+let cnt = 0;
+for (let i of arguments) {
+    if (i == "-cpy") {
         cnt++;
     }
-    else if(i[0]=="-"){
-       flags.push(i);
+    else if (i[0] == "-") {
+        flags.push(i);
     }
-    else if(i[0]=="%"){
+    else if (i[0] == "%") {
         secondaryarg.push(i.slice(1)); //$@ mean @ remove krdo dono file se $bhavesh se bhavesh remove
     }
-    else{
-       filenames.push(i);
+    else {
+        filenames.push(i);
     }
 }
 
@@ -34,81 +34,101 @@ for(let i of arguments){
 //                  let filedata=fs.readFileSync(files,"utf-8");
 //                      let filedataarray=filedata.split(" "); //array bnadiya string ka 
 //                       console.log(filedataarray.join(""));
-                 
+
 //             }
 //         }
 //      }
-    
+
 // }
 
 
-for(files of filenames){ //upper optimize code
-    let filedata=fs.readFileSync(files,"utf-8");
-    for(let flag of flags){
-        if(flag=="-rs"){   //remove spaces
-           filedata=filedata.split(" ").join("");
-        }
-        else if(flag=="-rn"){//remove lines
-            filedata=filedata.split("\n").join("");
-        }
-        else if(flag=="-rsc"){//remove special char  given in seconddary arg  &%*#@&1235
-         for(let argu of secondaryarg){
-             filedata= filedata.split(argu).join("");
-         }   
-        }
-        else if(flag=="-s"){     //numbering line
-            filedata=filedata.split("\n");
-            let tempd="";
-            
-            for(let i=0;i<filedata.length;i++){
-                 tempd+= `${i+1} ${filedata[i]}\n`;
-              }
-            filedata=tempd;
-              
-        }
-        else if(flag=="-sn"){ // numbering line by removing empty line
-            filedata=filedata.split("\n");
-            let j=0;
-            let tempd="";
-            for(let i=0;i<filedata.length;i++){
-                if(filedata[i]==""){
-                      j++;
-                      tempd+='\n';
-                }
-                else{
-                tempd+= `${i+1-j} ${filedata[i]}\n`;}
-             }
+for (files of filenames) { //upper optimize code
 
-             filedata=tempd;
-              
-        }
-        else if(flag=="-rel"){//remove extraline (if 2 or more continous only 1 shoudld remain)
-            filedata=filedata.split("\n");
-            let tempd="";
-            
-            for(let i=0;i<filedata.length;i++){
-                if(i<filedata.length-1 && filedata[i]=="" && filedata[i+1]==""){
-                     
+    if (fs.existsSync(files)) {
+
+        let filedata = fs.readFileSync(files, "utf-8");
+        for (let flag of flags) {
+            if (flag == "-rs") {   //remove spaces
+                filedata = filedata.split(" ").join("");
+            }
+            else if (flag == "-rn") {//remove lines
+                filedata = filedata.split("\n").join("");
+            }
+            else if (flag == "-rsc") {//remove special char  given in seconddary arg  &%*#@&1235
+                for (let argu of secondaryarg) {
+                    filedata = filedata.split(argu).join("");
                 }
-                else{
-                  tempd+= `${filedata[i]} \n`;}
+            }
+            else if (flag == "-s") {     //numbering line
+                filedata = filedata.split("\n");
+                let tempd = "";
+
+                for (let i = 0; i < filedata.length; i++) {
+                    tempd += `${i + 1} ${filedata[i]}\n`;
+                }
+                filedata = tempd;
+
+            }
+            else if (flag == "-sn") { // numbering line by removing empty line
+                filedata = filedata.split("\n");
+                let j = 0;
+                let tempd = "";
+                for (let i = 0; i < filedata.length; i++) {
+                    if (filedata[i] == "") {
+                        j++;
+                        tempd += '\n';
+                    }
+                    else {
+                        tempd += `${i + 1 - j} ${filedata[i]}\n`;
+                    }
+                }
+
+                filedata = tempd;
+
+            }
+            else if (flag == "-rel") {//remove extraline (if 2 or more continous only 1 shoudld remain)
+                filedata = filedata.split("\n");
+                let tempd = "";
+
+                for (let i = 0; i < filedata.length; i++) {
+                    if (i < filedata.length - 1 && filedata[i] == "" && filedata[i + 1] == "") {
+
+                    }
+                    else {
+                        tempd += `${filedata[i]} \n`;
+                    }
+                }
+
+                filedata = tempd;
             }
 
-            filedata=tempd;
+
+
+
         }
-        
 
-
-
+        console.log(filedata); //saare flag ka kaam krke phir print hoga
     }
-   
-    console.log(filedata); //saare flag ka kaam krke phir print hoga
+    else {
+        console.log(files + "  doesnt exist");
+    }
 }
-if(cnt>0){
-let data="";
-for(let i=1;i<filenames.length;i++){
-   data+= fs.readFileSync(filenames[i]);
-}
-fs.writeFileSync(filenames[0],data);
-console.log("file copied");
+if (cnt > 0) {
+    let data = "";
+    let cnt1 = 0;
+    for (let i = 1; i < filenames.length; i++) {
+        if (fs.existsSync(filenames[i])) {
+            data += fs.readFileSync(filenames[i]);
+            cnt1++;
+        }
+        else
+            console.log(filenames[i] + " doesnt exist");
+    }
+    if (cnt1 > 0) {
+        fs.writeFileSync(filenames[0], data);
+        console.log("file copied");
+    }
+    else {
+        console.log("files doesnt exist cant copy");
+    }
 }
